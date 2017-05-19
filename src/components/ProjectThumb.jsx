fa0d18b5a6ef09bf8444748b380821f73d274d31
Quentin Beauperin <quentin.beauperin@gmail.com>
2017-05-18 17:02:35 -0700

    Add routing logic, add new layout using Swiper

--- src/components/ProjectThumb.jsx
+++ src/components/ProjectThumb.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 class ProjectThumb extends React.Component {
     render() {
         return (
-            <div className="project">
-                <div className="wrap">
-                    <div className="image" style={{ backgroundImage: 'url('+this.props.img+')' }}></div>
-                    <div className="infos">
-                        <div className="center">
-                            <div className="name">{this.props.name}</div>
-                            <div className="techs">{this.props.techs}</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <NavLink
+                to={'/projects/' + this.props.uri}
+                key={this.props.key}
+                className="project swiper-slide"
+            >
+                <span className="wrap">
+                    <span className="image" style={{ backgroundImage: 'url('+this.props.img+')' }}></span>
+                    <span className="infos">
+                        <span className="center">
+                            <span className="name">{this.props.name}</span>
+                            <span className="techs">{this.props.techs}</span>
+                        </span>
+                    </span>
+                </span>
+            </NavLink>
         );
     }
 }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
-import Grid from '../components/Grid';
+// import Grid from '../components/Grid';
+import Swiper from 'react-id-swiper';
+import '../styles/swiper.css';
 import ProjectThumb from '../components/ProjectThumb';
 
 class Projects extends Component {
     render() {
         const projectsData = [
-            {key: 1, name: 'Leon and Harper', techs: 'magento, sass, grunt, bootstrap', 'img': 'https://unsplash.it/300/300/?random'},
-            {key: 2, name: 'Travel journal', techs: 'react, html5, sass', 'img': 'https://unsplash.it/300/300/?random'},
-            {key: 3, name: 'Leon and Harper', techs: 'react, html5, sass', 'img': 'https://unsplash.it/300/300/?random'},
-            {key: 4, name: 'Natalys', techs: 'react, html5, sass', 'img': 'https://unsplash.it/300/300/?random'},
-            {key: 5, name: 'Maille Leo', techs: 'react, html5, sass', 'img': 'https://unsplash.it/300/300/?random'},
-            {key: 6, name: 'Nisop', techs: 'react, html5, sass', 'img': 'https://unsplash.it/300/300/?random'},
-            {key: 7, name: 'Oxbow SUP', techs: 'react, html5, sass', 'img': 'https://unsplash.it/300/300/?random'},
-            {key: 8, name: 'Le Ptio', techs: 'react, html5, sass', 'img': 'https://unsplash.it/300/300/?random'}
+            {key: 1, name: 'Leon and Harper', 'uri': 'leon-and-harper', techs: 'magento, sass, grunt, bootstrap', 'img': 'https://unsplash.it/301/300/?random'},
+            {key: 2, name: 'Travel journal', 'uri': 'travel-journal', techs: 'react, html5, sass', 'img': 'https://unsplash.it/302/300/?random'},
+            {key: 3, name: 'Natalys', 'uri': 'natalys', techs: 'react, html5, sass', 'img': 'https://unsplash.it/300/301/?random'},
+            {key: 4, name: 'Maille Leo', 'uri': 'maille-leo', techs: 'react, html5, sass', 'img': 'https://unsplash.it/300/302/?random'},
+            {key: 5, name: 'Nisop', 'uri': 'nisop', techs: 'react, html5, sass', 'img': 'https://unsplash.it/301/301/?random'},
+            {key: 6, name: 'Oxbow SUP', 'uri': 'oxbow-sup', techs: 'react, html5, sass', 'img': 'https://unsplash.it/302/302/?random'},
+            {key: 7, name: 'Le Ptio', 'uri': 'le-ptio', techs: 'react, html5, sass', 'img': 'https://unsplash.it/301/302/?random'}
         ];
         const projects = projectsData.map((project) => (
             <ProjectThumb {...project}/>
         ));
+        const swiper = {
+            slidesPerView: 'auto',
+            centeredSlides: true
+        }
         return (
             <div className="projects">
-                <Grid elements={projects}/>
+                <Swiper {...swiper}>
+                    {projects}
+                </Swiper>
             </div>
         );
     }
