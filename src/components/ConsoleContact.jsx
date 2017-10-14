3fb3fc199a7aead8197460d0bfdec9969854885c
Quentin Beauperin <quentin.beauperin@gmail.com>
2017-10-13 19:59:08 -0700

    Fix contact email

--- src/components/ConsoleContact.jsx
+++ src/components/ConsoleContact.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class ConsoleContact extends React.Component {
     componentDidMount() {
         let basicStyle = 'background: #333; color: #333; font-size: 16px; line-height: 35px; padding: 15px 0;';
-        console.log("%c    You found the contact section, well done !     \n    So, now that you're here, here's my email :    \n    contact@[myfirstname][mylastname].com          ", basicStyle);
+        console.log("%c    You found the contact section, well done !     \n    So, now that you're here, here's my email :    \n    [myfirstname]@[domain]                         ", basicStyle);
     }
     render() {
         return null;
