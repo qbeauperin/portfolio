61ee9d73f0eb9090536512775b582acc985af8c7
Quentin Beauperin <quentin.beauperin@gmail.com>
2017-10-13 19:58:07 -0700

    Moved data into a separate file

--- src/App.js
+++ src/App.js
@@ -4,74 +4,11 @@ import Header from './components/Header';
 import Project from './components/Project';
 import ConsoleContact from './components/ConsoleContact';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import data from './data.js';
 
 class App extends Component {
     componentWillMount() {
-        this.projects = [
-            {
-                key: 1,
-                name: 'Leon and Harper',
-                uri: 'leon-and-harper',
-                techs: ['magento', 'sass', 'grunt', 'bootstrap'],
-                features: ['wordpress based API', 'dynamic google map with markers and polylines', 'responsive and multi-platform slider as main navigation', 'photo viewer'],
-                comments: ['some of the functionnalities have been abandoned during the development process for budget reasons'],
-                imgs: ['https://unsplash.it/1000/660/?random', 'https://unsplash.it/1000/665/?random']
-            },
-            {
-                key: 2,
-                name: 'Travel journal',
-                uri: 'travel-journal',
-                techs: ['react', 'html5', 'sass'],
-                features: ['wordpress based API', 'dynamic google map with markers and polylines', 'responsive and multi-platform slider as main navigation', 'photo viewer'],
-                comments: ['some of the functionnalities have been abandoned during the development process for budget reasons'],
-                imgs: ['https://unsplash.it/1000/661/?random', 'https://unsplash.it/1000/664/?random']
-            },
-            {
-                key: 3,
-                name: 'Natalys',
-                uri: 'natalys',
-                techs: ['react', 'html5', 'sass'],
-                features: ['wordpress based API', 'dynamic google map with markers and polylines', 'responsive and multi-platform slider as main navigation', 'photo viewer'],
-                comments: ['some of the functionnalities have been abandoned during the development process for budget reasons'],
-                imgs: ['https://unsplash.it/1000/662/?random', 'https://unsplash.it/1000/663/?random']
-            },
-            {
-                key: 4,
-                name: 'Maille Leo',
-                uri: 'maille-leo',
-                techs: ['react', 'html5', 'sass'],
-                features: ['wordpress based API', 'dynamic google map with markers and polylines', 'responsive and multi-platform slider as main navigation', 'photo viewer'],
-                comments: ['some of the functionnalities have been abandoned during the development process for budget reasons'],
-                imgs: ['https://unsplash.it/1000/663/?random', 'https://unsplash.it/1000/662/?random']
-            },
-            {
-                key: 5,
-                name: 'Nisop',
-                uri: 'nisop',
-                techs: ['react', 'html5', 'sass'],
-                features: ['wordpress based API', 'dynamic google map with markers and polylines', 'responsive and multi-platform slider as main navigation', 'photo viewer'],
-                comments: ['some of the functionnalities have been abandoned during the development process for budget reasons'],
-                imgs: ['https://unsplash.it/1000/664/?random', 'https://unsplash.it/1000/661/?random']
-            },
-            {
-                key: 6,
-                name: 'Oxbow SUP',
-                uri: 'oxbow-sup',
-                techs: ['react', 'html5', 'sass'],
-                features: ['wordpress based API', 'dynamic google map with markers and polylines', 'responsive and multi-platform slider as main navigation', 'photo viewer'],
-                comments: ['some of the functionnalities have been abandoned during the development process for budget reasons'],
-                imgs: ['https://unsplash.it/1000/662/?random', 'https://unsplash.it/1000/663/?random']
-            },
-            {
-                key: 7,
-                name: 'Le Ptio',
-                uri: 'le-ptio',
-                techs: ['react', 'html5', 'sass'],
-                features: ['wordpress based API', 'dynamic google map with markers and polylines', 'responsive and multi-platform slider as main navigation', 'photo viewer'],
-                comments: ['some of the functionnalities have been abandoned during the development process for budget reasons'],
-                imgs: ['https://unsplash.it/1000/660/?random', 'https://unsplash.it/1000/665/?random']
-            }
-        ];
+        this.projects = data.projects;
     }
     render() {
         return (
---
+++ src/data.js
@@ -0,0 +1,69 @@
+const data = {
+    'projects' : [
+        {
+            key: 1,
+            name: 'Leon and Harper',
+            uri: 'leon-and-harper',
+            techs: ['magento', 'sass', 'grunt', 'bootstrap'],
+            features: ['wordpress based API', 'dynamic google map with markers and polylines', 'responsive and multi-platform slider as main navigation', 'photo viewer'],
+            comments: ['some of the functionnalities have been abandoned during the development process for budget reasons'],
+            imgs: ['https://unsplash.it/1000/660/?random', 'https://unsplash.it/1000/665/?random']
+        },
+        {
+            key: 2,
+            name: 'Travel journal',
+            uri: 'travel-journal',
+            techs: ['react', 'html5', 'sass'],
+            features: ['wordpress based API', 'dynamic google map with markers and polylines', 'responsive and multi-platform slider as main navigation', 'photo viewer'],
+            comments: ['some of the functionnalities have been abandoned during the development process for budget reasons'],
+            imgs: ['https://unsplash.it/1000/661/?random', 'https://unsplash.it/1000/664/?random']
+        },
+        {
+            key: 3,
+            name: 'Natalys',
+            uri: 'natalys',
+            techs: ['react', 'html5', 'sass'],
+            features: ['wordpress based API', 'dynamic google map with markers and polylines', 'responsive and multi-platform slider as main navigation', 'photo viewer'],
+            comments: ['some of the functionnalities have been abandoned during the development process for budget reasons'],
+            imgs: ['https://unsplash.it/1000/662/?random', 'https://unsplash.it/1000/663/?random']
+        },
+        {
+            key: 4,
+            name: 'Maille Leo',
+            uri: 'maille-leo',
+            techs: ['react', 'html5', 'sass'],
+            features: ['wordpress based API', 'dynamic google map with markers and polylines', 'responsive and multi-platform slider as main navigation', 'photo viewer'],
+            comments: ['some of the functionnalities have been abandoned during the development process for budget reasons'],
+            imgs: ['https://unsplash.it/1000/663/?random', 'https://unsplash.it/1000/662/?random']
+        },
+        {
+            key: 5,
+            name: 'Nisop',
+            uri: 'nisop',
+            techs: ['react', 'html5', 'sass'],
+            features: ['wordpress based API', 'dynamic google map with markers and polylines', 'responsive and multi-platform slider as main navigation', 'photo viewer'],
+            comments: ['some of the functionnalities have been abandoned during the development process for budget reasons'],
+            imgs: ['https://unsplash.it/1000/664/?random', 'https://unsplash.it/1000/661/?random']
+        },
+        {
+            key: 6,
+            name: 'Oxbow SUP',
+            uri: 'oxbow-sup',
+            techs: ['react', 'html5', 'sass'],
+            features: ['wordpress based API', 'dynamic google map with markers and polylines', 'responsive and multi-platform slider as main navigation', 'photo viewer'],
+            comments: ['some of the functionnalities have been abandoned during the development process for budget reasons'],
+            imgs: ['https://unsplash.it/1000/662/?random', 'https://unsplash.it/1000/663/?random']
+        },
+        {
+            key: 7,
+            name: 'Le Ptio',
+            uri: 'le-ptio',
+            techs: ['react', 'html5', 'sass'],
+            features: ['wordpress based API', 'dynamic google map with markers and polylines', 'responsive and multi-platform slider as main navigation', 'photo viewer'],
+            comments: ['some of the functionnalities have been abandoned during the development process for budget reasons'],
+            imgs: ['https://unsplash.it/1000/660/?random', 'https://unsplash.it/1000/665/?random']
+        }
+    ]
+};
+
+export default data;
