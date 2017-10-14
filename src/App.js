b02fd8f0bdb330af7a199fad47af09f05a16dbe9
Quentin Beauperin <quentin.beauperin@gmail.com>
2017-10-13 19:58:57 -0700

    Add favicons meta

--- src/App.js
+++ src/App.js
@@ -14,9 +14,15 @@ class App extends Component {
         return (
             <Router>
                 <div className="App">
-                    <Helmet title="Quentin Beauperin" meta={[
-                        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-                    ]} />
+                    <Helmet title="Quentin Beauperin">
+                        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+                        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+                        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+                        <link rel="manifest" href="/manifest.json"/>
+                        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#222222"/>
+                        <meta name="theme-color" content="#ffffff"/>
+                    </Helmet>
                     <Route path="/" children={({ location }) => (
                         <Header location={location} projects={this.projects}/>
                     )}/>
