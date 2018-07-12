0f5cfded5cc01d5bc24120c526fd30f89c252210
Quentin Beauperin <quentin.beauperin@gmail.com>
2018-07-12 02:35:31 -0700

    Add data and screenshots

--- src/data.js
+++ src/data.js
@@ -4,64 +4,108 @@ const data = {
             key: 1,
             name: 'Leon and Harper',
             uri: 'leon-and-harper',
-            techs: ['magento', 'sass', 'grunt', 'bootstrap'],
-            features: ['wordpress based API', 'dynamic google map with markers and polylines', 'responsive and multi-platform slider as main navigation', 'photo viewer'],
-            comments: ['some of the functionnalities have been abandoned during the development process for budget reasons'],
-            imgs: ['https://unsplash.it/1000/660/?random', 'https://unsplash.it/1000/665/?random']
+            link: 'https://www.leonandharper.com/en/',
+            techs: ['magento', 'wordpress', 'fishpig', 'php', 'sass', 'grunt', 'bootstrap', 'gmaps'],
+            features: ['e-commerce', 'store locator', 'mobile version'],
+            imgs: [
+                {type: 'desktop', src: '/images/leonandharper/leonandharper1.jpg'},
+                {type: 'desktop', src: '/images/leonandharper/leonandharper2.jpg'},
+                {type: 'desktop', src: '/images/leonandharper/leonandharper3.jpg'},
+                {type: 'desktop', src: '/images/leonandharper/leonandharper4.jpg'},
+                {type: 'mobile', src: '/images/leonandharper/leonandharper5.jpg'},
+                {type: 'mobile', src: '/images/leonandharper/leonandharper6.jpg'},
+                {type: 'mobile', src: '/images/leonandharper/leonandharper7.jpg'},
+                {type: 'mobile', src: '/images/leonandharper/leonandharper8.jpg'},
+                {type: 'mobile', src: '/images/leonandharper/leonandharper9.jpg'},
+            ]
         },
         {
             key: 2,
             name: 'Travel journal',
             uri: 'travel-journal',
-            techs: ['react', 'html5', 'sass'],
-            features: ['wordpress based API', 'dynamic google map with markers and polylines', 'responsive and multi-platform slider as main navigation', 'photo viewer'],
-            comments: ['some of the functionnalities have been abandoned during the development process for budget reasons'],
-            imgs: ['https://unsplash.it/1000/661/?random', 'https://unsplash.it/1000/664/?random']
+            techs: ['react', 'html5', 'sass', 'wordpress'],
+            features: ['wordpress based API', 'dynamic google map with markers and polylines', 'responsive and multi-platform slider as main navigation', 'photo viewer', 'responsive'],
+            comments: ['Personal project to illustrate my cross-Canada road-trip'],
+            imgs: [
+                {type: 'desktop', src: '/images/traveljournal/traveljournal1.jpg'},
+                {type: 'desktop', src: '/images/traveljournal/traveljournal2.jpg'},
+                {type: 'mobile', src: '/images/traveljournal/traveljournal3.jpg'},
+                {type: 'mobile', src: '/images/traveljournal/traveljournal4.jpg'},
+            ]
         },
         {
             key: 3,
             name: 'Natalys',
             uri: 'natalys',
-            techs: ['react', 'html5', 'sass'],
-            features: ['wordpress based API', 'dynamic google map with markers and polylines', 'responsive and multi-platform slider as main navigation', 'photo viewer'],
-            comments: ['some of the functionnalities have been abandoned during the development process for budget reasons'],
-            imgs: ['https://unsplash.it/1000/662/?random', 'https://unsplash.it/1000/663/?random']
+            link: 'http://www.natalys.com/',
+            techs: ['magento', 'wordpress', 'fishpig', 'php', 'sass', 'grunt', 'bootstrap', 'gmaps'],
+            features: ['e-commerce', 'store locator', 'mobile version'],
+            imgs: [
+                {type: 'desktop', src: '/images/natalys/natalys1.jpg'},
+                {type: 'desktop', src: '/images/natalys/natalys2.jpg'},
+                {type: 'desktop', src: '/images/natalys/natalys3.jpg'},
+                {type: 'mobile', src: '/images/natalys/natalys4.jpg'},
+                {type: 'mobile', src: '/images/natalys/natalys5.jpg'},
+                {type: 'mobile', src: '/images/natalys/natalys6.jpg'},
+            ]
         },
         {
             key: 4,
             name: 'Maille Leo',
             uri: 'maille-leo',
-            techs: ['react', 'html5', 'sass'],
-            features: ['wordpress based API', 'dynamic google map with markers and polylines', 'responsive and multi-platform slider as main navigation', 'photo viewer'],
-            comments: ['some of the functionnalities have been abandoned during the development process for budget reasons'],
-            imgs: ['https://unsplash.it/1000/663/?random', 'https://unsplash.it/1000/662/?random']
+            techs: ['javascript', 'jquery', 'sass'],
+            features: ['custom t-shirt configurator', 'responsive'],
+            comments: [
+                'the configurator allowed the user to create a custom t-shirt with the choice of sleeve type, collar type, color but also hundreds of designs to choose from and place wherever (front, back, sleeve and/or stack them)',
+                'the user could see in real time multiple views of the shirt based on 3D renders and then place an order when satisfied with his creation',
+                'I wish I knew React then'
+            ],
+            imgs: [
+                {type: 'desktop', src: '/images/mailleleo/mailleleo1.jpg'},
+                {type: 'desktop', src: '/images/mailleleo/mailleleo2.jpg'},
+                {type: 'desktop', src: '/images/mailleleo/mailleleo3.jpg'},
+                {type: 'desktop', src: '/images/mailleleo/mailleleo4.jpg'},
+            ]
         },
         {
             key: 5,
             name: 'Nisop',
             uri: 'nisop',
-            techs: ['react', 'html5', 'sass'],
-            features: ['wordpress based API', 'dynamic google map with markers and polylines', 'responsive and multi-platform slider as main navigation', 'photo viewer'],
-            comments: ['some of the functionnalities have been abandoned during the development process for budget reasons'],
-            imgs: ['https://unsplash.it/1000/664/?random', 'https://unsplash.it/1000/661/?random']
+            link: 'https://www.nisop.fr/',
+            techs: ['wordpress', 'php', 'html5', 'sass'],
+            features: ['courses catalog', 'online reservation', 'interesting design', 'cute little logo animation'],
+            imgs: [
+                {type: 'desktop', src: '/images/nisop/nisop1.jpg'},
+                {type: 'desktop', src: '/images/nisop/nisop2.jpg'},
+                {type: 'mobile', src: '/images/nisop/nisop3.jpg'},
+                {type: 'mobile', src: '/images/nisop/nisop4.jpg'},
+            ]
         },
         {
             key: 6,
             name: 'Oxbow SUP',
             uri: 'oxbow-sup',
-            techs: ['react', 'html5', 'sass'],
-            features: ['wordpress based API', 'dynamic google map with markers and polylines', 'responsive and multi-platform slider as main navigation', 'photo viewer'],
-            comments: ['some of the functionnalities have been abandoned during the development process for budget reasons'],
-            imgs: ['https://unsplash.it/1000/662/?random', 'https://unsplash.it/1000/663/?random']
+            link: 'http://www.oxbow-sup.com/ambassadeurs/',
+            techs: ['wordpress', 'php', 'html5', 'sass'],
+            features: ['differents object types presentation (ambassadors, collections, events, news)', 'user contribution module', 'newsletter'],
+            imgs: [
+                {type: 'desktop', src: '/images/oxbowsup/oxbowsup1.jpg'},
+                {type: 'desktop', src: '/images/oxbowsup/oxbowsup2.jpg'},
+                {type: 'desktop', src: '/images/oxbowsup/oxbowsup3.jpg'},
+                {type: 'desktop', src: '/images/oxbowsup/oxbowsup4.jpg'},
+            ]
         },
         {
             key: 7,
             name: 'Le Ptio',
             uri: 'le-ptio',
-            techs: ['react', 'html5', 'sass'],
-            features: ['wordpress based API', 'dynamic google map with markers and polylines', 'responsive and multi-platform slider as main navigation', 'photo viewer'],
-            comments: ['some of the functionnalities have been abandoned during the development process for budget reasons'],
-            imgs: ['https://unsplash.it/1000/660/?random', 'https://unsplash.it/1000/665/?random']
+            link: 'http://imf-le-ptio.fr/personnaliser',
+            techs: ['php', 'cakephp', 'javascript', 'jquery', 'sass'],
+            features: ['custom scooter configurator', '360° live preview', 'purchase order generation'],
+            imgs: [
+                {type: 'desktop', src: '/images/leptio/leptio1.jpg'},
+                {type: 'desktop', src: '/images/leptio/leptio2.jpg'},
+            ]
         }
     ]
 };
